a5f169899dc124e274423d60e21dee0b70c39b3e
riwantositinjak <riwanto1212@gmail.com>
2022-11-29 14:30:28 +0700

    styling list item cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,9 +3,9 @@ import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
-    <ul>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li></li>
+        <li>{item.name}</li>
       ))}
     </ul>
   );
