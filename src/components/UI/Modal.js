478ef02b995d901943c5ce260df55db70779ba00
riwantositinjak <riwanto1212@gmail.com>
2022-11-29 14:09:05 +0700

    adding modal using react portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom";
+import React, { Fragment } from "react";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}/>;
+};
+
+const Overlay = (props) => {
+  return <div className={classes.modal}>
+    <div className={classes.content}>{props.children}</div>
+  </div>;
+};
+
+const portalTag = document.getElementById("overlay");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalTag)}
+      {ReactDOM.createPortal(<Overlay>{props.children}</Overlay>, portalTag)}
+    </Fragment>
+  );
+};
+
+export default Modal;
